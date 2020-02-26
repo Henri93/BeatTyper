ebcde27b862bad6ccc3dffbfd46c23bf36be1c4c
Henri93 <henryrgarant@gmail.com>
2020-02-26 16:33:39 -0500

    pls netifly work

---
+++ build/precache-manifest.29ccfa8daf0685e1f7a890d3092958d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a13b43b5f3b6e6bfca6a3c2fd648c559",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9a1e30c7cf2b84a977e",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "e9a1e30c7cf2b84a977e",
+    "url": "/static/js/2.1048cf3b.chunk.js"
+  },
+  {
+    "revision": "b8714132b55319d9985b269752700df3",
+    "url": "/static/js/2.1048cf3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "251e4e72014df37ec63f",
+    "url": "/static/js/main.c7a315dc.chunk.js"
+  },
+  {
+    "revision": "7e6d8c24c5d3b82fe6be",
+    "url": "/static/js/runtime-main.6f88f9a7.js"
+  }
+]);
